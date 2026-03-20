a888f3a980cef732f8248f48cf03fd3ea712b598
Venkatram Harish Belvadi <hello@vhbelvadi.com>
2026-03-20 15:11:08 +0000

    Updated weidget for Statamic 6

--- resources/views/widgets/webmentions.blade.php
+++ resources/views/widgets/webmentions.blade.php
@@ -1,5 +1,5 @@
-<div class="h-full card p-0">
-    <header class="flex justify-between items-center p-4 border-b dark:bg-dark-650 dark:border-b dark:border-dark-900">
+<div class="bg-white dark:bg-gray-850 rounded-xl ring ring-gray-200 dark:ring-x-0 dark:ring-b-0 dark:ring-gray-700/80 shadow-ui-md @container/widget min-h-54" data-ui-card data-inset="true">
+    <header class="flex items-center min-h-[49px] justify-between border-b border-gray-200 px-4.5 py-2 dark:border-gray-700">
         <div class="flex gap-2">
             <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="text-gray-800 dark:text-dark-200" stroke="currentColor" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg> -->
             <svg id="Star-Messages--Streamline-Atlas" class="text-gray-800 dark:text-dark-200" xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 24 24" height="24" width="24"><desc>Star Messages Streamline Icon: https://streamlinehq.com</desc><defs></defs><path d="m14.240833333333333 8.36625 0.8529166666666668 1.81125 1.8975 0.2875 -1.3704166666666666 1.40875 0.31625000000000003 1.98375 -1.69625 -0.9391666666666667 -1.69625 0.9391666666666667 0.32583333333333336 -1.98375L11.5 10.465l1.8975 -0.2875 0.8433333333333334 -1.81125z" fill="currentColor" stroke-width="1"></path><path d="M21.5625 9.631250000000001v10.0625l-2.7408333333333332 -2.7504166666666667h-8.232083333333334a3.65125 3.65125 0 0 1 -3.545833333333334 -2.7408333333333332 3.612916666666667 3.612916666666667 0 0 1 -0.115 -0.9583333333333334v-3.612916666666667a3.660833333333333 3.660833333333333 0 0 1 3.660833333333333 -3.660833333333333h7.312083333333334a3.660833333333333 3.660833333333333 0 0 1 3.660833333333333 3.660833333333333Z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></path><path d="M16.07125 5.050416666666666v0.9583333333333334h-5.4816666666666665a3.660833333333333 3.660833333333333 0 0 0 -3.660833333333333 3.660833333333333v2.7408333333333332H4.178333333333334L1.4375 15.112916666666667V5.050416666666666a3.660833333333333 3.660833333333333 0 0 1 3.660833333333333 -3.660833333333333H12.458333333333334a3.660833333333333 3.660833333333333 0 0 1 3.612916666666667 3.660833333333333Z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></path></svg>
@@ -11,12 +11,12 @@
         @endif
     </header>
     @if ( $mentions )
-        <table class="data-table">
-            <tbody tabindex="0">
+        <table class="w-full text-sm">
+            <tbody tabindex="0" class="block py-2 w-full">
                 @foreach ($mentions as $mention)
-                    <tr class="sortable-row outline-none">
+                    <tr class="grid sortable-row w-full">
                         <td>
-                            <div class="flex items-center justify-between gap-2">
+                            <div class="flex items-center justify-between gap-2 px-4 py-2">
                                 <span class="flex flex-col items-start justify-start inline flex-wrap">
                                     <a class="text-sm block" href="{{ explode(' ', $mention->get_description())[0] }}" target="_blank">
                                         <strong>
